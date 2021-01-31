afaa849bb869ca3449d81b0da9be9881e57f2199
Dayan Yamin <yamindayan@gmail.com>
2021-01-31 19:16:19 -0400

    quick fixes/cleanup

--- client/src/components/Question/AddQuestion.js
+++ client/src/components/Question/AddQuestion.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 
-
-
 class AddQuestion extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +10,7 @@ class AddQuestion extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
   handleSubmit(event) {
     alert("A name was submitted: " + this.state.value);
@@ -22,27 +20,23 @@ class AddQuestion extends React.Component {
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address (Opcional)</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Direccion Email (Opcional)</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Permite respuestas privadas."
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Pregunta</Form.Label>
           <Form.Control type="text" placeholder="text" />
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
-  );
+    );
   }
 }
-
 
 export default AddQuestion;
--- client/src/components/Question/ViewQuestion.js
+++ client/src/components/Question/ViewQuestion.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
-import { Form, Button } from "react-bootstrap";
 import AddAnswer from "../Answer/AddAnswer";
 
-// const { useEffect, useState } = require("react");
 
 //View for a single question.
 //Display answers if any
@@ -11,8 +9,6 @@ const ViewQuestion = (props) => {
   if (typeof props.location.state === "undefined") {
     return <Redirect to="/" />;
   }
-
-  // const [questions, setQuestions] = useState([]);
   return (
     <React.Fragment>
       <div>
@@ -20,7 +16,7 @@ const ViewQuestion = (props) => {
           <p>{props.location.state.question.text}</p>
         </h3>
 
-       <AddAnswer state={ props.location.state.question.id } / >
+        <AddAnswer state={props.location.state.question.id} />
       </div>
     </React.Fragment>
   );
