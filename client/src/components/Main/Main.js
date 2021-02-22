b20a75d51f0f5c0f1ce8a98a700687ade01637fe
Dayan Yamin <yamindayan@gmail.com>
2021-02-22 10:25:44 -0400

    Pagination feature added

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import AddQuestion from "../Question/AddQuestion.js";
 import Categories from "./Categories.js";
 import CardQuestion from "../Question/CardQuestion.js";
+import Pagination from "react-js-pagination";
 
 const { useEffect, useState } = require("react");
 
@@ -11,6 +12,22 @@ function Main() {
   }, []);
 
   const [questions, setQuestions] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const questionsPerPage = 10;
+  const pagesVisited = (pageNumber - 1) * questionsPerPage;
+  const pageCount = Math.ceil(questions.length / questionsPerPage);
+  
+
+  const displayQuestionsWithPagination = questions
+    .slice(pagesVisited, pagesVisited + questionsPerPage)
+    .map((question) => {
+      return (
+        <div key={question._id}>
+          <CardQuestion state={question} />
+        </div>
+      );
+    });
+
   const fetchQuestions = async () => {
     const data = await fetch(process.env.REACT_APP_DB_URL + "/question");
     const questions = await data.json();
@@ -21,17 +38,24 @@ function Main() {
     const newQuestions = [question].concat(questions);
     setQuestions(newQuestions);
   };
+
+  const handlePageChange = selected => {
+    setPageNumber(selected);
+  };
   return (
     <React.Fragment>
       <AddQuestion setQuestionsChild={updateQuestions} />
       <Categories />
-      <div>
-        {questions.map((question) => (
-          <div key={question._id}>
-            <CardQuestion state={question} />
-          </div>
-        ))}
-      </div>
+      {displayQuestionsWithPagination}
+      <Pagination
+        itemClass="page-item"
+        linkClass="page-link"
+        activePage={pageNumber}
+        itemsCountPerPage={questionsPerPage}
+        totalItemsCount={questions.length}
+        pageRangeDisplayed={pageCount}
+        onChange={handlePageChange}
+      />
     </React.Fragment>
   );
 }
