a6b1a97947308bbad83117aac4b43882e550bbb7
Dayan Yamin <yamindayan@gmail.com>
2021-03-02 23:45:03 -0400

    Cleanup firestore functions

--- firebase/functions/controllers/question.js
+++ firebase/functions/controllers/question.js
@@ -1,28 +1,28 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable indent */
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
 
 const questionApp = express();
-questionApp.use(cors({origin: true}));
-questionApp.use(bodyParser.json());
+questionApp.use(cors({ origin: true }));
 
 // Retrieve all questions
 questionApp.get("/", async (req, res) => {
   const snapshot = await db
-      .collection("questions")
-      .orderBy("date", "desc")
-      .get();
+    .collection("questions")
+    .orderBy("date", "desc")
+    .get();
   const questions = [];
   snapshot.forEach((doc) => {
     const id = doc.id;
     const data = doc.data();
     questions.push({
-      id,
+      _id: id,
       ...data,
     });
   });
@@ -35,16 +35,22 @@ questionApp.post("/add", async (req, res) => {
   const question = {
     date,
     ...req.body,
+    answers: [],
   };
   await db.collection("questions").add(question);
   res.status(201).send(JSON.stringify(question));
 });
 
-questionApp.put("/addanswer/:questionId", async (req, res) => {
+questionApp.patch("/addanswer/:questionId", async (req, res) => {
+  const date = admin.firestore.Timestamp.now();
   const body = req.body;
-  await db.collection("questions").doc(req.params.id).update({
-    body,
+  const questionRef = await db
+    .collection("questions")
+    .doc(req.params.questionId.toString());
+  await questionRef.update({
+    answers: admin.firestore.FieldValue.arrayUnion({ date, ...body }),
   });
-  res.status(200).send();
+  const data = (await questionRef.get().then()).data();
+  res.status(200).send(JSON.stringify(data));
 });
 exports.questions = functions.https.onRequest(questionApp);
