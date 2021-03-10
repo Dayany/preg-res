ccb3d9a41f247726b4cd7d36e8eb010a149fe1db
Dayan Yamin <yamindayan@gmail.com>
2021-03-10 14:35:39 -0400

    Cleanup Warnings

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,35 +2,23 @@ import React from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    {
-      return (
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#">PregRes</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <NavDropdown title="Language">
-              <Nav.Link
-                onClick={() => this.props.setLocaleChild("en")}
-                href="#"
-              >
-                English
-              </Nav.Link>
-              <Nav.Link
-                onClick={() => this.props.setLocaleChild("es")}
-                href="#"
-              >
-                Español
-              </Nav.Link>
-            </NavDropdown>
-          </Navbar.Collapse>
-        </Navbar>
-      );
-    }
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#">PregRes</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <NavDropdown title="Language">
+            <Nav.Link onClick={() => this.props.setLocaleChild("en")} href="#">
+              English
+            </Nav.Link>
+            <Nav.Link onClick={() => this.props.setLocaleChild("es")} href="#">
+              Español
+            </Nav.Link>
+          </NavDropdown>
+        </Navbar.Collapse>
+      </Navbar>
+    );
   }
 }
 
