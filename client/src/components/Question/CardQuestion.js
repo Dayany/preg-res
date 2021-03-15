a981ed5c7d7692df7a70d4cabb7f33b97e3dc8c4
Dayan Yamin <yamindayan@gmail.com>
2021-03-15 19:12:23 -0400

    Make the whole card a link

--- client/src/components/Question/CardQuestion.js
+++ client/src/components/Question/CardQuestion.js
@@ -5,34 +5,34 @@ class CardQuestion extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div
-          style={{ margin: "5px" }}
-          className="card border-primary h-100"
+        <Link
           key={this.props.state.id}
+          to={{
+            pathname: `/question`,
+            state: {
+              questionId: this.props.state.id,
+              firestore: this.props.firestore,
+            },
+          }}
         >
           <div
-            className="card-body d-flex flex-column align-items-start"
+            style={{ margin: "5px" }}
+            className="card border-primary h-100"
             key={this.props.state.id}
           >
-            <h4
-              className="card-title text-primary ng-binding"
+            <div
+              className="card-body d-flex flex-column align-items-start"
               key={this.props.state.id}
             >
-              <Link
+              <h4
+                className="card-title text-primary ng-binding"
                 key={this.props.state.id}
-                to={{
-                  pathname: `/question`,
-                  state: {
-                    questionId: this.props.state.id,
-                    firestore: this.props.firestore,
-                  },
-                }}
               >
                 {this.props.state.text}
-              </Link>
-            </h4>
+              </h4>
+            </div>
           </div>
-        </div>
+        </Link>
       </React.Fragment>
     );
   }
